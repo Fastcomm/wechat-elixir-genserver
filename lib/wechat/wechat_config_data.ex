1130ffe0aa2506324c4c732c0109c4dcca1325a7
Carel-Ttrumpet <carel@ttrumpet.com>
2017-03-15 14:32:54 +0200

    Get default token and aes_key from config file

--- lib/wechat/wechat_config_data.ex
+++ lib/wechat/wechat_config_data.ex
@@ -2,7 +2,7 @@ defmodule Wechat.ConfigData do
 
   defstruct appid: nil,
             secret: nil,
-            token: nil,
-            encoding_aes_key: nil,
+            token: Application.get_env(:wechat, Wechat)[:token],
+            encoding_aes_key: Application.get_env(:wechat, Wechat)[:encoding_aes_key],
             token_file: "/tmp/access_token"
   end
